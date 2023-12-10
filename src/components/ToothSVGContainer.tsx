3d400a4f5a5fedb3f4dbb15f4cea4282fa08e589
shuhei <be.imaginative.oct.26@gmail.com>
2023-12-10 19:38:58 +0900

    betagaki

--- src/components/ToothSVGContainer.tsx
+++ src/components/ToothSVGContainer.tsx
@@ -37,7 +37,7 @@ const ToothSVGContainer = ({
   };
 
   const svgFileName = `${jawType}_${surface[0]}_${toothNumber[1]}.svg`;
-  const svgFileURL = `${BASE_PATH}/assets/${svgFileName}`;
+  const svgFileURL = `/periodontal-chart/assets/${svgFileName}`;
   const imageWidth = TOOTH_SVG_FILE_WIDTHS[svgFileName];
   const imageHeight = surface !== "occlusal" ? 120 : 45;
 
