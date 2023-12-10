f1d13eeee607b113ea66f284635bf7bb3f846cec
shuhei <be.imaginative.oct.26@gmail.com>
2023-12-10 18:49:34 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   distDir: "./out",
   output: "export",
   basePath: process.env.GITHUB_ACTIONS ? "/github.io-periodontal-chart" : "",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
