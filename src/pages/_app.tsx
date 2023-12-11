32f7f77eb7f712a336f7fdb2ed19ff921a013be7
shuhei <be.imaginative.oct.26@gmail.com>
2023-12-11 23:33:52 +0900

    fix GA4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 import "@styles/styles.scss";
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from '@/lib/gtag';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  return (
+    <>
+      <GoogleAnalytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
