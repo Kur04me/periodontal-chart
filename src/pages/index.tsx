d33ca7c9159694249164ad0e050a8437ad51f758
YOSHIDA <be.imaginative.oct.26@gmail.com>
2023-12-14 15:41:56 +0900

    Set title and meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,24 @@ import Content from "@/components/Content";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Menu from "@/components/Menu";
+import Head from "next/head";
 
 const Home = () => {
   return (
-    <div id="App">
-      <Menu />
-      <div id="main-container">
-        <Header />
-        <Content />
-        <Footer />
+    <>
+    <Head>
+      <title>Periodontal Chart</title>
+      <meta name="description" content="歯周組織検査表を作れます" />
+    </Head>
+      <div id="App">
+        <Menu />
+        <div id="main-container">
+          <Header />
+          <Content />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
