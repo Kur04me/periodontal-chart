0eadf7e7627e2371e5be0cbd279e706bfe0ee99a
YOSHIDA <be.imaginative.oct.26@gmail.com>
2023-12-21 22:19:07 +0900

    [setting]SEO
    Set keywords and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,15 @@ export default function Document() {
     <Html lang="ja">
       <Head>
         <meta name="description" content="歯周組織検査表を作れます" />
-        <meta name="google-site-verification" content="xMvlKc-uEIfoV84LZtib4bRsx7zv71DyHkgHWVcY0PI" />
+        <meta
+          name="google-site-verification"
+          content="xMvlKc-uEIfoV84LZtib4bRsx7zv71DyHkgHWVcY0PI"
+        />
+        <meta
+          name="keywords"
+          content="歯周病, 歯周組織検査, 無料, オンラインツール, 歯周病管理"
+        />
+        <link rel="icon" type="image/ico" href="/src/app/icon.ico" />
       </Head>
       <body>
         <Main />
