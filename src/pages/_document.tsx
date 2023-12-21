a02587c7d5d7a1e53c79147a0edef5318984f2c5
YOSHIDA <be.imaginative.oct.26@gmail.com>
2023-12-21 12:52:41 +0900

    add google search console tag

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ja">
+      <Head>
+        <meta name="description" content="歯周組織検査表を作れます" />
+        <meta name="google-site-verification" content="xMvlKc-uEIfoV84LZtib4bRsx7zv71DyHkgHWVcY0PI" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
